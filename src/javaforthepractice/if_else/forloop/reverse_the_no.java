fcece64daf778ebb54795ed22eaef0636f88ddc5
renav2 <kulkarnirenav255@gmail.com>
2021-06-23 22:55:40 +0530

    revesr int

---
+++ src/javaforthepractice/if_else/forloop/reverse_the_no.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaforthepractice.if_else.forloop;
+import java.util.Scanner;
+
+/**
+ *
+ * @author rvkul
+ */
+public class reverse_the_no {
+   public static void main(String args[]){
+Scanner in =new Scanner(System.in);
+
+int a = 
+
+
+   }
+}
