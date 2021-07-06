aa311d7f1ed7e653070297b4b7543af7734254b3
renav2 <kulkarnirenav255@gmail.com>
2021-07-06 22:34:07 +0530

    1 to 10 table using for loop

---
+++ src/forloop/table1to10.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forloop;
+
+/**
+ *
+ * @author rvkul
+ */
+public class table1to10 {
+    public static void main(String args[]){
+    
+       for(int a=1;a<=10;a++){
+        for(int i=1;i<=10;i++){
+            System.out.println(a+" * "+i+" = "+a*i);
+        }
+           System.out.println("");
+       }
+    
+    }
+}
