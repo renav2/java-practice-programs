52ea910be1df85c35d1380a236a20a18a95147b1
renav2 <kulkarnirenav255@gmail.com>
2021-07-08 23:06:21 +0530

    sumoftheno

--- src/whileloop/sumoftheno.java
+++ src/whileloop/sumoftheno.java
@@ -5,10 +5,29 @@
  */
 package whileloop;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rvkul
  */
 public class sumoftheno {
+    public static void main(String args[]){
     
+  Scanner in=new Scanner(System.in);
+  int a=in.nextInt();
+  
+  int temp=0;
+  int rev;
+  
+  while(a!=0){
+  rev=a%10;
+  a/=10;
+  temp +=rev;
+  
+  }
+        System.out.println(temp);
+  
+    
+    }
 }
