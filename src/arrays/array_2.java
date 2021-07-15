75e9fe05e5454aa85c7f7c47a5db9bc7332f20a5
renav2 <kulkarnirenav255@gmail.com>
2021-07-15 17:15:27 +0530

    array2

--- src/arrays/array_2.java
+++ src/arrays/array_2.java
@@ -4,11 +4,37 @@
  * and open the template in the editor.
  */
 package arrays;
+import java.util.*;
 
 /**
  *
  * @author rvkul
  */
 public class array_2 {
+    //first try to  perform  to insert array 
+public static void main(String args[]){
+ 
+    
+        int len, p,ele;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length of an array:");
+        len = sc.nextInt();
+        int arr[] = new int[len+1];
+        System.out.println("Enter "+len+" elements:");
+        for(int i = 0; i < len; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter the element which you want to insert:");
+        ele = sc.nextInt();
+        arr[len] = ele;
+        System.out.print("After inserting : ");
+        for(int i = 0; i <len; i++)
+        {
+            System.out.print(arr[i]+",");
+        }
+        System.out.print(arr[len]);
+        
+    }
     
 }
