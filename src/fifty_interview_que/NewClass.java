71f4d9656853f07fd988ca7473bc052578a6013e
renav2 <kulkarnirenav255@gmail.com>
2021-07-20 22:34:30 +0530

    going for array again

---
+++ src/fifty_interview_que/NewClass.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fifty_interview_que;
+import java.util.*;
+
+/**
+ *
+ * @author rvkul
+ */
+public class NewClass {
+ public static void main(String args[]){
+ Scanner in=new Scanner(System.in);
+     System.out.println("enter how many row u want ");
+ int a=in.nextInt();
+ for(int i=1;i<=a;i++){
+ 
+ for(int j=1;j<=i;j++){
+     System.out.print("*");
+ }
+     System.out.println("");
+ 
+ }
+ 
+ 
+ 
+ }   
+}
