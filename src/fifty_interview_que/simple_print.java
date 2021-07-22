b180714c03f34275e69ef52c7a1d81d453315d63
renav2 <kulkarnirenav255@gmail.com>
2021-07-22 17:30:56 +0530

    elusion

---
+++ src/fifty_interview_que/simple_print.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fifty_interview_que;
+
+/**
+ *
+ * @author rvkul
+ */
+public class simple_print {
+     public static void main (String args[])   
+    {  
+        System.out.println(10 + 20 + "Javatpoint");   
+        System.out.println("Javatpoint" + 10 + 20);  
+    }  
+}
