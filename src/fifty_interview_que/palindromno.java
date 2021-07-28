2e2d8338966144d0dcf015bbeedd90ae1dd2e3b9
renav2 <kulkarnirenav255@gmail.com>
2021-07-28 22:21:33 +0530

    et1

---
+++ src/fifty_interview_que/palindromno.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fifty_interview_que;
+import java.util.*;
+
+/**
+ *
+ * @author rvkul
+ */
+public class palindromno {
+public static void main(String args[]){
+
+
+
+}    
+}
