74891c89c8e9a87de30652be6cfb407be4d3f639
renav2 <kulkarnirenav255@gmail.com>
2021-07-28 16:41:08 +0530

    leap

--- src/fifty_interview_que/largest.java
+++ src/fifty_interview_que/largest.java
@@ -4,11 +4,29 @@
  * and open the template in the editor.
  */
 package fifty_interview_que;
-
+import java.awt.BorderLayout;
+import java.util.*;
 /**
  *
  * @author rvkul
  */
 public class largest {
+    public static void main(String args[]){
+    // largest no
+    Scanner in=new Scanner(System.in);
+    int a=in.nextInt();
+        int b=in.nextInt();
+            int c=in.nextInt();
     
+    if(a>b &&a>c){
+        System.out.println("a is largest no");
+    
+    }else if(b>a && b>c){
+        System.out.println(" b is largest among the no");
+    }else{
+    
+        System.out.println("c is greter no");
+    }
+    
+    }
 }
