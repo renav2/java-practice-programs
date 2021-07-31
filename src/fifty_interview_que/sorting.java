3cefc37d41bf5277225c1152e5ed5b2c4635313c
renav2 <kulkarnirenav255@gmail.com>
2021-07-31 16:50:09 +0530

    liner search

--- src/fifty_interview_que/sorting.java
+++ src/fifty_interview_que/sorting.java
@@ -9,44 +9,42 @@ package fifty_interview_que;
  *
  * @author rvkul
  */
-class a1a{
-void sorting(int arr[]){
+class a1a {
+
+    void sorting(int arr[]) {
 //sort    
-for(int i=0;i<arr.length;i++)
-{
-   int  temp=0;
-for(int j=0;j<arr.length-i-1;j++){
-if(arr[j]>arr[j+1]){
-temp=arr[j];
-arr[j]=arr[j+1];
-arr[j+1]=temp;
+        for (int i = 0; i < arr.length; i++) {
+            int temp = 0;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
 
-}
-}
-    
-}
-System.out.println("sorted array");
-    
-    for(int j=0;j<arr.length;j++){
-        System.out.println(arr[j]);
-    }
-    
+                }
+            }
+
+        }
+        System.out.println("sorted array");
+
+        for (int j = 0; j < arr.length; j++) {
+            System.out.println(arr[j]);
+        }
+
 //print array
-
-
-
+    }
 
 }
 
-}
 public class sorting {
-    public static void main(String args[]){
-    a1a obj=new a1a();
-    int arr[]= {1,33,23,11,4,5,7};
-    System.out.println("orignal array");
-for(int i=0;i<arr.length;i++){
-    System.out.println(arr[i]);
-}
-    obj.sorting(arr);
+
+    public static void main(String args[]) {
+        a1a obj = new a1a();
+        int arr[] = {1, 33, 23, 11, 4, 5, 7};
+        System.out.println("orignal array");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        obj.sorting(arr);
     }
 }
