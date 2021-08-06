19c464360598f30c1ad81fceaa556d99f28bcf73
renav2 <kulkarnirenav255@gmail.com>
2021-08-06 10:20:30 +0530

    element present in array

---
+++ src/fifty_interview_que/element_present_in_array_or_not.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fifty_interview_que;
+
+/**
+ *
+ * @author rvkul
+ */
+public class element_present_in_array_or_not {
+    public static void main(String args[]){
+    int a[]={44,4324,4,6,81,12,6};
+    for(int i=0;i<a.length;i++){
+    if(a[i]==99){
+        System.out.println("elemnt is present");
+            break;
+    }
+ 
+    }
+    
+    }
+}
