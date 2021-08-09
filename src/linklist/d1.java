99ba55b04df1f971cd36363a75a0e14af5f7a14a
renav2 <kulkarnirenav255@gmail.com>
2021-08-09 17:37:29 +0530

    link1 try1

--- src/linklist/d1.java
+++ src/linklist/d1.java
@@ -10,5 +10,41 @@ package linklist;
  * @author rvkul
  */
 public class d1 {
+    private linklist head;
+    private static class linklist{
+    private int data;
+    private linklist next;
     
+    public linklist(int data){
+    this.data=data;
+    this.next=null;
+    
+    }
+   }
+    public void display(){
+    linklist temp=head;
+    while(temp!=null){
+        System.out.print(temp.data+"->");
+        temp=temp.next;
+        
+    }
+        System.out.println("");
+    }
+    
+    
+    
+    
+    
+    
+    public static void main(String args[]){
+    d1 obj=new d1();
+    obj.head=new linklist(30);
+    linklist sec=new linklist(20);
+    linklist thir=new linklist(54);
+    obj.head.next=sec;
+    sec.next=thir;
+    obj.display();
+    
+    
+    }
 }
