599ca9b29e9b95a044ca955b7b79a1df21f2b5e4
renav2 <kulkarnirenav255@gmail.com>
2021-08-09 23:04:09 +0530

    single link list done

--- src/linklist/singlelinklist.java
+++ src/linklist/singlelinklist.java
@@ -10,5 +10,48 @@ package linklist;
  * @author rvkul
  */
 public class singlelinklist {
+    private  linknode head;
+
     
-}
+    
+    private static class linknode{
+    private int data ;
+    private linknode next;
+    
+    
+    public linknode(int data){
+    this.data=data;
+    this.next=null;
+    }}
+   public  void display(){
+    linknode temp=head;
+    while(temp!=null){
+        System.out.print(temp.data+"->");
+        temp=temp.next;
+    }
+        System.out.println("");
+    
+    
+    
+    }
+    
+    
+    
+    public static void main(String args[]){
+     singlelinklist obj= new singlelinklist();
+     obj.head=new linknode(10);
+     linknode fir= new linknode(20);
+     linknode sec=new linknode(25);
+     
+     //connecting node with
+     obj.head.next=fir;
+     fir.next=sec;
+     
+    obj.display();
+    
+    
+    }
+    }
+    
+    
+
