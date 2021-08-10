8633f5b8079210ba3a6f24161129d1289da42508
renav2 <kulkarnirenav255@gmail.com>
2021-08-10 15:32:21 +0530

    done 1 st position

---
+++ src/linklist/inser_satar_end.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linklist;
+import java.util.*;
+
+/**
+ *
+ * @author rvkul
+ */
+public class inser_satar_end {
+    private link head;
+
+    private static class link {
+
+        private int data;
+        private link next;
+
+        public link(int data) {
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+    //display
+    void show(){
+    link temp=head;
+    while(temp!=null){
+        System.out.print(temp.data+"->");
+        temp=temp.next;
+        
+    }
+    
+    }
+    void star(){
+        Scanner in=new Scanner(System.in);
+        System.out.println("enter the new data ");        
+int new_data=in.nextInt();
+
+    link new_node=new link(new_data);
+  new_node.next=head;
+  head=new_node;
+  
+    
+    }
+    
+    
+   public static void main(String args[]){
+   inser_satar_end obj=new inser_satar_end();
+   obj.head=new link(39);
+   link sec=new link(57);
+   link thir =new link(90);
+   
+   //
+   obj.head.next=sec;
+   sec.next=thir;
+   obj.show();
+   obj.star();
+   obj.show();
+   } 
+}
